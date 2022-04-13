e30224534f6f01075deac486b98f73a1d9b2f3ab
ZK <zk@zkdeveloper.com>
2022-04-13 11:48:13 +0300

    Add font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from "next/app";
 import GlobalWrapper from "../theme";
 import Head from "next/head";
+import { GoogleFonts } from "next-google-fonts";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <GoogleFonts href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;800;900&family=Montserrat:wght@300;400;500;600;800;900&display=swap" />
       <Head>
         <title>Tgrade</title>
         <link rel="icon" href="/favicon.ico" />
