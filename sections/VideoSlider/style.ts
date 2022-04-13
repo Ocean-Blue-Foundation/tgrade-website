3917f4d719c198648ffe8b31091833b3ff4dc109
ZK <zk@zkdeveloper.com>
2022-04-14 02:08:17 +0300

    Slider mobile styles

--- sections/VideoSlider/style.ts
+++ sections/VideoSlider/style.ts
@@ -1,4 +1,5 @@
 import styled, { css } from "styled-components";
+import { device } from "../../theme/Breakpoints";
 
 export const StyledContainer = styled.div`
   padding: 10rem 0;
@@ -16,6 +17,10 @@ export const StyledWrapper = styled.div`
   .slick-slide {
     width: 100%;
     height: 100%;
+
+    @media ${device.tablet} {
+      min-height: 14.8rem;
+    }
   }
 
   .slick-active {
@@ -23,11 +28,23 @@ export const StyledWrapper = styled.div`
   }
 
   .slick-slide > div {
-    margin: 0 10px;
+    margin: 0 1rem;
+
+    @media ${device.mobile} {
+      margin: 0 3rem;
+      margin: 0 3rem 0 0;
+    }
   }
   .slick-list {
     overflow: hidden;
-    margin: 0 -10px;
+    margin: 0 -1rem;
+
+    @media ${device.mobile} {
+      margin: 0 3rem 0 0;
+      display: flex;
+      padding: 0 20% 0 0 !important;
+      overflow: initial;
+    }
   }
 
   .slick-dots {
@@ -38,11 +55,16 @@ export const StyledWrapper = styled.div`
     column-gap: 1.4rem;
     list-style-type: none;
     margin-top: 12.2rem;
+    position: relative;
+
+    @media ${device.tablet} {
+      margin-top: 7.2rem;
+    }
 
     button {
       display: block;
-      width: 6px;
-      height: 6px;
+      width: 0.6rem;
+      height: 0.6rem;
       padding: 0;
       border: none;
       border-radius: 100%;
@@ -53,8 +75,8 @@ export const StyledWrapper = styled.div`
 
     .slick-active button {
       background-color: var(--color-white);
-      width: 10px;
-      height: 10px;
+      width: 1rem;
+      height: 1rem;
       border: 2px solid var(--color-black);
     }
   }
@@ -64,17 +86,31 @@ export const StyledTopContent = styled.div`
   display: flex;
   justify-content: space-between;
   margin-bottom: 6rem;
+
+  @media ${device.tablet} {
+    flex-direction: column;
+    margin-bottom: 4rem;
+  }
 `;
 
 export const heading = css`
   font-size: 3rem;
   line-height: 5rem;
   font-weight: 700;
+
+  @media ${device.tablet} {
+    padding-bottom: 1rem;
+  }
+
+  @media ${device.mobile} {
+    font-size: 2rem;
+    line-height: 3.4rem;
+  }
 `;
 
 export const videoHeading = css`
-  font-size: 18px;
-  line-height: 22px;
+  font-size: 1.8rem;
+  line-height: 2.2rem;
   color: #8692a6;
   padding-top: 2rem;
   padding-bottom: 1rem;
