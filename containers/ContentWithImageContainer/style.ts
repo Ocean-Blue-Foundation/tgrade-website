13b148174e2f62f436c0d6a035f94eb6100b1101
ZK <zk@zkdeveloper.com>
2022-04-14 02:08:53 +0300

    Update section mobile styles

--- containers/ContentWithImageContainer/style.ts
+++ containers/ContentWithImageContainer/style.ts
@@ -6,4 +6,9 @@ export const StyledContainer = styled.div`
   row-gap: 200px;
   padding: 160px 0;
   flex-direction: column;
+
+  @media ${device.tablet} {
+    padding: 5rem 0 0;
+    row-gap: 5rem;
+  }
 `;
