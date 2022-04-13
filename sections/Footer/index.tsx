98826efaa9049db0c2dc3ec852eecebba3c2f8a1
ZK <zk@zkdeveloper.com>
2022-04-13 11:52:55 +0300

    Add footer

---
+++ sections/Footer/index.tsx
@@ -0,0 +1,39 @@
+import { StyledEmailLink, FooterContainer, Wrapper, FooterNav, FooterContent, logo } from "./style";
+import MainContainer from "../../containers/MainContainer";
+import FooterLink from "../../components/FooterLink";
+import SvgLogo from "../../components/SvgLogo";
+import SocialIcons from "../../components/SocialIcons";
+import ReusableStyles from "../../theme/ReusableStyles";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <MainContainer>
+        <FooterContent>
+          <Wrapper>
+            <SvgLogo css={logo} />
+            <p css={ReusableStyles.paragraphWhite}>
+              This website is maintained by Confio. The contents and opinions of this website are those of
+              Confio and might not be consessus in the Tgrade ecosystem.
+            </p>
+          </Wrapper>
+          <FooterNav>
+            <FooterLink text="Privacy Policy" link="/privacy-policy" />
+            <FooterLink text="Press kit" link="/press-kit" />
+            <FooterLink text="Impressum" link="/impressum" />
+          </FooterNav>
+          <Wrapper>
+            <p css={ReusableStyles.paragraphWhite}>Contact us</p>
+            <Link href="mailto:hello@confio.gmbh" passHref={true}>
+              <StyledEmailLink>hello@confio.gmbh</StyledEmailLink>
+            </Link>
+            <SocialIcons />
+          </Wrapper>
+        </FooterContent>
+      </MainContainer>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
