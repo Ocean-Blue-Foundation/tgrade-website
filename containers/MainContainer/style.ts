690d2cf5d7eefba7e882128e390431eeed6c5515
ZK <zk@zkdeveloper.com>
2022-04-13 11:47:44 +0300

    Add containers

---
+++ containers/MainContainer/style.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import { device } from "../../theme/Breakpoints";
+import { setSize } from "../../theme/Functions";
+
+export const StyledContainer = styled.div`
+  padding: 0 ${setSize(100)};
+  width: 100%;
+  height: 100%;
+  max-width: 1440px;
+  margin: 0 auto;
+
+  @media ${device.laptop} {
+    padding: 0 10rem;
+  }
+
+  @media ${device.tablet} {
+    padding: 0 1rem;
+  }
+`;
