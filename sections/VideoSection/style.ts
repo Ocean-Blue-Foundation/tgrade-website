38450291ab8e7ebfa9d375d983056b89b7fe372e
ZK <zk@zkdeveloper.com>
2022-04-14 02:10:35 +0300

    Update video section styles

--- sections/VideoSection/style.ts
+++ sections/VideoSection/style.ts
@@ -4,23 +4,40 @@ import { device } from "../../theme/Breakpoints";
 export const StyledContainer = styled.div`
   padding: 164px 0 216px;
   background: rgba(228, 231, 243, 0.7);
+
+  @media ${device.tablet} {
+    padding: 5rem 0 4rem;
+  }
 `;
 
 export const StyledWrapper = styled.div`
   display: flex;
   column-gap: 90px;
+
+  @media ${device.tablet} {
+    flex-direction: column;
+  }
 `;
 
 export const StyledRightContent = styled.div`
   display: flex;
   flex-direction: column;
   width: 46.9%;
+
+  @media ${device.tablet} {
+    width: 100%;
+  }
 `;
 
 export const StyledLeftContent = styled.div`
   display: flex;
   flex-direction: column;
   width: 53.1%;
+
+  @media ${device.tablet} {
+    width: 100%;
+    padding-bottom: 3rem;
+  }
 `;
 
 export const heading = css`
@@ -28,6 +45,12 @@ export const heading = css`
   line-height: 5rem;
   padding-bottom: 2rem;
   font-weight: 700;
+
+  @media ${device.mobile} {
+    font-size: 2rem;
+    line-height: 3.4rem;
+    padding-bottom: 1rem;
+  }
 `;
 
 export const StyledVideo = styled.div`
@@ -52,6 +75,10 @@ export const StyledLink = styled.div`
 
   &:not(:first-child) {
     padding-top: 2.4rem;
+
+    @media ${device.mobile} {
+      padding-top: 1.1rem;
+    }
   }
 `;
 
