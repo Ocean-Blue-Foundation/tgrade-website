665bfde4e5e015afdb93a712ca021db44a920de9
ZK <zk@zkdeveloper.com>
2022-07-12 10:21:01 +0300

    update main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,16 @@
 import type { NextPage } from "next";
 import FullBackgroundImage from "../sections/FullBackgroundImage";
-import ContentWithImage from "../sections/ContentWithImage";
-import SubscribeSection from "../sections/SubscribeSection";
+import FullBackgroundVideo from "../sections/FullBackgroundVideo";
 import Partner from "../sections/Partner";
-import VideoSection from "../sections/VideoSection";
-import VideoSlider from "../sections/VideoSlider";
-import Features from "../sections/Features";
 import ReusableStyles from "../theme/ReusableStyles";
-import ContentWithImageContainer from "../containers/ContentWithImageContainer";
 import BtnLink from "../components/BtnLink";
+import cn from "../utils/cn";
 import MainLayout from "../layouts/MainLayout";
 import useScrollSpy from "react-use-scrollspy";
 import { useRef } from "react";
 
 const Home: NextPage = () => {
-  const sectionRefs = [
-    useRef(null),
-    useRef(null),
-    useRef(null),
-    useRef(null),
-    useRef(null),
-    useRef(null),
-    useRef(null),
-  ];
+  const sectionRefs = [useRef(null), useRef(null), useRef(null), useRef(null), useRef(null), useRef(null)];
 
   const activeHeaderLink = useScrollSpy({
     sectionElementRefs: sectionRefs,
@@ -32,64 +20,15 @@ const Home: NextPage = () => {
   return (
     <MainLayout activeHeaderLink={activeHeaderLink}>
       <section id="welcome" ref={sectionRefs[0]}>
-        <FullBackgroundImage
-          image="main-bg.jpg"
-          headingText="The Big Bang of Regulated Blockchain"
-          subHeadingText="Welcome To Tgrade"
+        <FullBackgroundVideo
+          video="hero-video-bg.mp4"
+          headingText="Business solving real-world problems"
           textColor="var(--color-white)"
-          responsive={true}
+          maxWidth={100}
         >
-          <p css={ReusableStyles.paragraphWhite}>
-            Our vision is for self-sovereign groups, bound by their local jurisdiction and regulations,
-            trading P2P programmable financial instruments.
-          </p>
-        </FullBackgroundImage>
-      </section>
-      <section id="problem" ref={sectionRefs[1]}>
-        <ContentWithImageContainer>
-          <ContentWithImage
-            image="/bitcoin.jpg"
-            headingText="DeFi has grown explosively since 2020, roughly $200bn in total value locked."
-            subHeadingText="Problem"
-            headingLevel={3}
-          >
-            <p>
-              People involved in the blockchain and DeFi world have a libertarian mind-set who seek to avoid
-              regulations.
-            </p>
-          </ContentWithImage>
-          <ContentWithImage
-            image="/markets.jpg"
-            headingText="The traditional markets volumes are thousands of trillions."
-            headingLevel={3}
-            imageLeft={true}
-          >
-            <p>
-              Financial institutions cannot enter the DeFi world as they need to know who they are trading
-              with and the origin of funds. A public, permissionless blockchain can be problematic for
-              financial institutions unless a whitelisting system is in place.
-            </p>
-          </ContentWithImage>
-          <ContentWithImage
-            image="/balance.jpg"
-            headingText="How do we balance decentralization, innovation and a framework to support regulated business?"
-            headingLevel={3}
-          />
-        </ContentWithImageContainer>
-      </section>
-      <section id="solution" ref={sectionRefs[2]}>
-        <FullBackgroundImage
-          image="solution-bg.jpg"
-          headingText="The answer is Tgrade!"
-          subHeadingText="Solution"
-          textColor="var(--color-white)"
-          headingLevel={2}
-        >
-          <p css={ReusableStyles.paragraphWhite}>
-            We build a self-sovereign framework which ensures the principal of decentralisation. Institutions
-            within a given jurisdiction can each a self-sovereign group and ensure they comply with their
-            local regulations. On-chain financial instruments controlled by self-sovereign groups, and
-            on-chain governance complying with corresponding off-chain regulations is the key solution.
+          <p css={cn(ReusableStyles.paragraphWhite, ReusableStyles.maxWidth1, ReusableStyles.paragraphBig)}>
+            Imagine real-world problems, solved by innovative businesses,{" "}
+            <span css={ReusableStyles.boldWhite}>powered by the Tgrade blockchain</span>.
           </p>
           <BtnLink
             text="Go to the Tgrade App"
@@ -98,35 +37,116 @@ const Home: NextPage = () => {
             borderColor="var(--color-white)"
             backgroundColor="var(--color-white)"
           />
+        </FullBackgroundVideo>
+      </section>
+      <section id="people" ref={sectionRefs[1]}>
+        <FullBackgroundImage
+          image="people-bg.jpg"
+          headingText="To bring people together"
+          textColor="var(--color-black)"
+          headingLevel={2}
+          align="center"
+          verticalAlign="start"
+        >
+          <p css={cn(ReusableStyles.paragraphBlack, ReusableStyles.maxWidth2)}>
+            We want to drive the adoption of blockchain technology by making it usable and useful in the real
+            world. We are solving real-world problems by{" "}
+            <span css={ReusableStyles.emphasizedText}>bringing together people, businesses</span>, and{" "}
+            <span css={ReusableStyles.emphasizedText}>blockchain</span>.
+          </p>
         </FullBackgroundImage>
       </section>
-      <section id="how-it-works" ref={sectionRefs[3]}>
-        <VideoSection />
-      </section>
-      <section id="features" ref={sectionRefs[4]}>
-        <Features />
-        <Partner headingText="Investor & Strategic partner" />
-      </section>
-      <section id="knowledge" ref={sectionRefs[5]}>
-        <VideoSlider />
-      </section>
-      <section id="game-of-engagement" ref={sectionRefs[6]}>
+      <section id="platform" ref={sectionRefs[2]}>
         <FullBackgroundImage
-          image="engagement-bg.jpg"
-          headingText="Game of Engagement 2022"
+          image="platform-bg.jpg"
+          headingText="A Revolutionary Platform"
+          textColor="var(--color-black)"
+          headingLevel={2}
+          align="center"
+          maxWidth={90}
+        >
+          <p css={ReusableStyles.paragraphBlack}>
+            Tgrade is built using revolutionary blockchain technology, smart contracts, and new forms of
+            decentralized autonomous organization. <span css={ReusableStyles.emphasizedText}>We solve</span>{" "}
+            the basic problem in public chains of pseudo anonymous addresses and{" "}
+            <span css={ReusableStyles.emphasizedText}>
+              the issues of not knowing who you are trading with
+            </span>{" "}
+            through Trusted Circles, a self-sovereign governance mechanism to allow you to create your own
+            groups with your own rules.
+          </p>
+          <BtnLink
+            text="Try Tgrade App"
+            link="https://try.tgrade.finance/"
+            color="var(--color-white)"
+            borderColor="var(--color-black)"
+            backgroundColor="var(--color-black)"
+          />
+        </FullBackgroundImage>
+        <FullBackgroundImage
+          image="safe-bg.jpg"
+          headingText="We give businesses a safe blockchain home"
+          textColor="var(--color-black)"
+          headingLevel={2}
+          align="right"
+          maxWidth={51}
+        >
+          <p css={ReusableStyles.paragraphBlack}>
+            Tgrade is a home for businesses. By providing a central hub for commerce, and a neutral foundation
+            for businesses to interact with one another and their customers,{" "}
+            <span css={ReusableStyles.emphasizedText}>
+              we are helping to build a robust ecosystem for businesses to thrive in the new era of blockchain
+              technology.
+            </span>
+          </p>
+        </FullBackgroundImage>
+      </section>
+      <section id="business" ref={sectionRefs[3]}>
+        <FullBackgroundVideo
+          video="business-video-bg.mp4"
+          headingText="Revolutionize your business with blockchain!"
           textColor="var(--color-white)"
           headingLevel={2}
         >
-          <p css={ReusableStyles.paragraphWhite}>
+          <BtnLink
+            text="Try Tgrade App"
+            link="https://try.tgrade.finance/"
+            color="var(--color-black)"
+            borderColor="var(--color-white)"
+            backgroundColor="var(--color-white)"
+          />
+        </FullBackgroundVideo>
+      </section>
+      <section id="partner" ref={sectionRefs[4]}>
+        <Partner headingText="Investor & Strategic partner" />
+      </section>
+      <section id="game-of-engagement" ref={sectionRefs[5]}>
+        <FullBackgroundImage
+          image="engagement-bg.jpg"
+          headingText="Play a Game of Tgrade Engagement 2022"
+          textColor="var(--color-white)"
+          headingLevel={2}
+          align="center"
+          maxWidth={88}
+        >
+          <p css={cn(ReusableStyles.paragraphWhite, ReusableStyles.maxWidth3)}>
             Following the launch of Tgrade there will be a Game of Engagement which will be run over a 4 week
             period. There will be double engagement points on offer, a series of challenges, some will be
             announced in advance to help people with planning and others will be flash challenges on the same
             time on each Monday morning.
           </p>
-          <p css={ReusableStyles.paragraphWhite}>Dates and challenges will be announced in Q2 2022.</p>
+          <p css={cn(ReusableStyles.paragraphWhite, ReusableStyles.maxWidth3)}>
+            Dates and challenges will be announced in Q2 2022.
+          </p>
+          <BtnLink
+            text="Send a Request"
+            link="https://try.tgrade.finance/"
+            color="var(--color-black)"
+            borderColor="var(--color-white)"
+            backgroundColor="var(--color-white)"
+          />
         </FullBackgroundImage>
       </section>
-      <SubscribeSection textColor="var(--color-white)" headingText="Sign up for Tgrade Mailing List" />
     </MainLayout>
   );
 };
