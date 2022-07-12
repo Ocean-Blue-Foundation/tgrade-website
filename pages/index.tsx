d96d4bfa433f8ba2707f42ed69e3023e972bd095
ZK <zk@zkdeveloper.com>
2022-07-12 10:31:42 +0300

    Replace link

--- pages/index.tsx
+++ pages/index.tsx
@@ -138,13 +138,13 @@ const Home: NextPage = () => {
           <p css={cn(ReusableStyles.paragraphWhite, ReusableStyles.maxWidth3)}>
             Dates and challenges will be announced in Q2 2022.
           </p>
-          <BtnLink
+          {/* <BtnLink
             text="Send a Request"
             link="https://try.tgrade.finance/"
             color="var(--color-black)"
             borderColor="var(--color-white)"
             backgroundColor="var(--color-white)"
-          />
+          /> */}
         </FullBackgroundImage>
       </section>
     </MainLayout>
