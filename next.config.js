f082e87f96afaf7fbe58d83071569cf319c91532
HannuProd <Hannubit@gmail.com>
2022-12-07 14:16:39 +0100

    removed config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
-const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
 
-const assetPrefix = `/${repo}/`;
-const basePath = `/${repo}`;
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: assetPrefix,
-  basePath: basePath,
 };
 
 module.exports = nextConfig;
